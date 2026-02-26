96e7cc799bdaa89c8cf54c01c71644c2c17097df
lglickman-edu <lglickman@uchicago.edu>
2026-02-26 04:48:22 +0000

    boards show

--- app/controllers/boards_controller.rb
+++ app/controllers/boards_controller.rb
@@ -2,4 +2,7 @@ class BoardsController < ApplicationController
   def index
     render({ :template => "board_templates/index"})
   end
+  def show
+    render({ :template => "board_templates/show"})
+  end
 end
--- config/routes.rb
+++ config/routes.rb
@@ -2,4 +2,6 @@ Rails.application.routes.draw do
   # This is a blank app! Pick your first screen, build out the RCAV, and go from there. E.g.:
   # get("/your_first_screen", { :controller => "pages", :action => "first" })
   get("/", { :controller => "boards", :action => "index"})
+  get("/boards/:path_id", { :controller => "boards", :action => "show"})
+  
 end
